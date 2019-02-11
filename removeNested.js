972638e9739e66718b260d00e42d47e8ca79324b
Noemi Rozpara <nrozpara@gmail.com>
2019-02-11 16:26:16 +0100

    refactor: use closure to split arguments
    Now function findKeyValue can be ran using more handy string argument (e.g. 'name.first'), not array

--- removeNested.js
+++ removeNested.js
@@ -1,25 +1,29 @@
-export const findKey = (object, keys) => {
-  if (keys && keys[0]) {
-    const currentKey = keys[0];
-    const currentElement = object[currentKey];
-    if (currentElement) {
-      if (typeof currentElement === 'object' && Object.keys(currentElement)) {
-        return findKey(currentElement, [keys.pop()]);
+export const findKeyValue = (object, keys) => {
+  const splittedKeys = keys.split('.');
+
+  const findValue = (objectToSearchIn, keysArray) => {
+    if (keysArray && keysArray[0]) {
+      const currentKey = keysArray[0];
+      const currentElement = objectToSearchIn[currentKey];
+      if (currentElement) {
+        if (typeof currentElement === 'object' && Object.keys(currentElement)) {
+          return findValue(currentElement, [keysArray.pop()]);
+        }
+        return currentElement;
       }
-      return currentElement;
+      return NaN;
     }
     return NaN;
-  }
-  return NaN;
+  };
+
+  return findValue(object, splittedKeys);
 };
 
 export const removeFromArray = (array = [], condition, value = null) => {
   const index = condition
-    ? array.findIndex(item => findKey(item, condition.split('.')) === value)
+    ? array.findIndex(item => findKeyValue(item, condition) === value)
     : array.findIndex(item => item === value);
-  const returnedArray =
-    index > -1
-      ? [...array.slice(0, index), ...array.slice(index + 1)]
-      : [...array];
-  return returnedArray;
+  return index > -1
+    ? [...array.slice(0, index), ...array.slice(index + 1)]
+    : array;
 };
